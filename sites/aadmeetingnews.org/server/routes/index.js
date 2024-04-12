3731d0cdff0292a4685aea8572f61cd082d7496c
Brian Miller <brian@parameter1.com>
2024-04-12 10:29:25 -0500

    Revert Route logic and update dynamic header logic instead
    It will now look for the presents of the nativeX publishes sponsor id as well.

--- sites/aadmeetingnews.org/server/routes/index.js
+++ sites/aadmeetingnews.org/server/routes/index.js
@@ -2,7 +2,6 @@ const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
 const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
-const nativeX = require('./native-x');
 const search = require('./search');
 const websiteSections = require('./website-section');
 
@@ -12,9 +11,6 @@ module.exports = (app) => {
 
   // Homepage
   home(app);
-
-  // Site Native X route
-  nativeX(app);
 
   // Dynamic Pages
   dynamicPages(app);
--- sites/aadmeetingnews.org/server/routes/native-x.js
+++
@@ -1,9 +0,0 @@
-const queryFragment = require('@ascend-media/package-global/graphql/fragments/native-x-story');
-const template = require('@ascend-media/package-global/templates/content/native-x-story');
-const withNativeXStory = require('@parameter1/base-cms-marko-web-native-x/middleware/with-story');
-const { getAsObject } = require('@parameter1/base-cms-object-path');
-
-module.exports = (app) => {
-  const config = getAsObject(app, 'locals.nativeX');
-  app.get('/aesthetics/sponsored/:section/:slug/:id', withNativeXStory({ config, template, queryFragment }));
-};
