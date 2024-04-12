c098f5408ff431dc0e7b8902a175fa693fbd55bd
Brian Miller <brian@parameter1.com>
2024-04-12 09:58:11 -0500

    Add publisher branded routeing for AAD

--- sites/aadmeetingnews.org/server/routes/index.js
+++ sites/aadmeetingnews.org/server/routes/index.js
@@ -2,6 +2,7 @@ const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
 const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
+const nativeX = require('./native-x');
 const search = require('./search');
 const websiteSections = require('./website-section');
 
@@ -11,6 +12,9 @@ module.exports = (app) => {
 
   // Homepage
   home(app);
+
+  // Site Native X route
+  nativeX(app);
 
   // Dynamic Pages
   dynamicPages(app);
